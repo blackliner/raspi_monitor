7f0d1533cad3ee61b5ab77ba0dcf1f56b3c50898
Florian Berchtold <florian.berchtold@luminartech.com>
2021-04-20 22:11:32 +0000

    add install

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 set -euxo pipefail
 
+#install vcgencmd
+sudo apt update
+sudo apt install -ylibraspberrypi-bin
+
 sudo python3 -m pip install .
 
 sudo cp raspi_monitor.service /etc/systemd/system
